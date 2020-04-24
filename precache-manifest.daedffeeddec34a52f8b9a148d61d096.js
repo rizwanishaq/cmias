1d9eace09fc93b6b968c3db934a329a48c082bd4
rizwanishaq <rizwanishaq@gmail.com>
2020-04-24 18:16:02 +0200

    Updates

---
+++ precache-manifest.daedffeeddec34a52f8b9a148d61d096.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e115d84b27115a7e7e2bbefeeb643e4f",
+    "url": "/cmias/index.html"
+  },
+  {
+    "revision": "b502242917c3595b521d",
+    "url": "/cmias/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "5536cfcf7520d52a3cc5",
+    "url": "/cmias/static/js/2.9e2bdaaa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cmias/static/js/2.9e2bdaaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b502242917c3595b521d",
+    "url": "/cmias/static/js/main.0457a095.chunk.js"
+  },
+  {
+    "revision": "6ccbd8e0732b72ac528b",
+    "url": "/cmias/static/js/runtime-main.01cfe966.js"
+  }
+]);
